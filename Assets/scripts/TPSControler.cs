99ef4123ecfe449da55707796ac679517c06ce73
zerojoan <zerojoan117@gmail.com>
2023-10-06 13:30:49 +0200

    camaras

---
+++ Assets/scripts/TPSControler.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSControler : MonoBehaviour
+{
+   private CharacterController _controller;
+   private Transform _camera;
+
+    private float _horizontal;
+
+    private float _vertical;
+
+    //variables para salto y gravedad
+
+    [SerializeField] private float _playerspeed = 5;
+
+    [SerializeField] private float _jumpHeight = 1;
+   
+    private float _gravity = -9.81f;
+    private Vector3 _playerGravity;
+    //Variables para rotacion
+
+    private float turnSmoothVelocity;
+
+    [SerializeField]  float turnSmoothTime = 0.1f;
+
+    //variables para sensor
+
+    [SerializeField] private Transform _sensorPosition;
+    [SerializeField] private float _sensorRadius = 0.2f;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private bool _isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetButton("Fire2"))
+        {
+            AimMovement();
+        }
+        else 
+        {
+            Movement();
+        }
+       
+        Jump();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+
+        if(direction != Vector3. zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _controller.Move(moveDirection.normalized * _playerspeed * Time.deltaTime);
+
+        }
+            
+    }
+
+    void AimMovement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+
+         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+         float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _camera.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+
+         transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+
+        if(direction != Vector3. zero)
+        {
+           
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _controller.Move(moveDirection.normalized * _playerspeed * Time.deltaTime);
+
+        }
+            
+    }
+    
+
+    
+    void Jump()
+    {
+
+        _isGrounded = Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+
+        if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = 0;
+
+        }
+        if(_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+        }
+
+        _playerGravity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_playerGravity * Time.deltaTime);
+    }
+}
